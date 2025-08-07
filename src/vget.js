452a02301c5d6f4a0471aada6b561182aac63e3a
veypi <i@veypi.com>
2025-08-07 21:01:56 +0800

    update axios default timeout

--- src/vget.js
+++ src/vget.js
@@ -34,7 +34,7 @@ async function getEnv(root, temp) {
       $bus: new EventBus(),
       $axios: axios.create({
         baseURL: baseURL,
-        timeout: 3000,
+        timeout: 10000,
       }),
       $message: vmessage,
       $router: null,
